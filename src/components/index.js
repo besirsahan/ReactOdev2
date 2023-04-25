0201ab9bfbd6292a7c062ea948708b446a4f85cd
besirsahan <besirsahan@gmail.com>
2023-04-25 07:46:51 +0300

    index.js, input.js, addList.js completed

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./input";
 import AddList from "./addList";
 import FooterEnd from "./footerEnd";
 
 function Index() {
+  const [todos, setTodos] = useState([]);
   return (
     <div>
-      index
-      <hr />
-      <Input />
-      <hr />
-      <AddList />
-      <hr />
+      <section
+        mv-app="todoapp"
+        class="todoapp"
+        mv-bar="none"
+        mv-storage="local"
+        mv-autosave="3"
+        mv-mode="edit"
+        mv-init="#data"
+      >
+        <Input setTodos={setTodos} todos={todos} />
+        <section class="main">
+          <input class="toggle-all" type="checkbox" />
+          <label for="toggle-all">Mark all as complete</label>
+          <AddList todos={todos} setTodos={setTodos} />
+        </section>
+      </section>
       <FooterEnd />
-      <hr />
     </div>
   );
 }
--- src/components/input.js
+++ src/components/input.js
@@ -1,46 +1,36 @@
 import React, { useState, useEffect } from "react";
 
-function Input() {
-  const [todo, setTodo] = useState("");
-  const changeTodo = (e) => {
-    setTodo(e.target.value);
-  };
+function Input({ todos, setTodos }) {
+  const [value, setValue] = useState("");
   const [id, setId] = useState(0);
+  const changeTodo = (e) => {
+    setValue(e.target.value);
+  };
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       e.preventDefault();
       setId(id + 1);
-      setTodo([...todo, { id: id, text: todo, completed: false }]);
-      setTodo("");
+      setTodos([...todos, { id: id, text: value, completed: false }]);
+      setValue("");
     }
   };
 
   return (
     <div>
-      <section
-        mv-app="todoapp"
-        class="todoapp"
-        mv-bar="none"
-        mv-storage="local"
-        mv-autosave="3"
-        mv-mode="edit"
-        mv-init="#data"
-      >
-        <header className="header">
-          <h1>todos</h1>
-          <form>
-            <input
-              className="new-todo"
-              property="newTodo"
-              placeholder="What needs to be done?"
-              value={todo}
-              onKeyDown={handleKeyDown}
-              onChange={changeTodo}
-              autoFocus
-            />
-          </form>
-        </header>
-      </section>
+      <header className="header">
+        <h1>todos</h1>
+        <form>
+          <input
+            className="new-todo"
+            property="newTodo"
+            placeholder="What needs to be done?"
+            value={value}
+            onKeyDown={handleKeyDown}
+            onChange={changeTodo}
+            autoFocus
+          />
+        </form>
+      </header>
     </div>
   );
 }
